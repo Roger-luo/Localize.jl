113ffc2e363ff1090cc517147202bd7e65c1b30b
Roger-luo <hiroger@qq.com>
2018-07-19 23:50:12 +0800

    add fr, tw example

--- example/example.jl
+++ example/example.jl
@@ -2,10 +2,24 @@ using Localize
 
 @i18n Base.Math begin
 
-@lang zh_CN """
+@lang zh"""
     sin(x)
 
 正弦函数
+"""
+sin
+
+@lang zh_TW """
+    sin(x)
+
+正弦函數
+"""
+sin
+
+@lang fr"""
+    sin(x)
+
+fonction sinusoïdale
 """
 sin
 
